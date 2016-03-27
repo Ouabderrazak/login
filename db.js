3fc8d204c823be2dd13c099da4c9e7d5c5275106
Ouabderrazak <youssef.ouabderrazak@gmail.com>
2016-03-27 19:02:41 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var Bookshelf = require('bookshelf');
 
 var config = {
-   host: 'localhost',  // your host
-   user: 'root', // your database user
-   password: '', // your database password
+   host: 'localhost',  //  host
+   user: 'root', //  database user
+   password: '', //  database password
    database: 'dbUsers',
    charset: 'UTF8_GENERAL_CI'
 };
